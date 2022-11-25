32975419103bdad281205a75415e7cf908a80c6d
Luis Felipe Garção <luis.silva@unimedprudente.com.br>
2022-11-25 09:14:54 -0300

    config head

--- alterarSenha.php
+++ alterarSenha.php
@@ -35,19 +35,7 @@ if (isset($_POST['re_password'])) {
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-  <meta charset="UTF-8">
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
-
-  <link rel="stylesheet" href="styles/styles.css">
-  <link rel="stylesheet" href="styles/login.css">
-  <title>Alterar Senha</title>
-</head>
+<?php include('head.php') ?>
 
 <style>
   .header-custom {
